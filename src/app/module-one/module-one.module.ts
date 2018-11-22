917108f769ae49a89a410778527706175b904a36
Jorge Cano <jorgeucano@gmail.com>
2018-11-22 10:29:32 -0300

    add service

---
+++ src/app/module-one/module-one.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModuleOneRoutingModule } from './module-one-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModuleOneRoutingModule
+  ]
+})
+export class ModuleOneModule { }
---
+++ src/app/module-shared/module-shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModuleSharedRoutingModule } from './module-shared-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModuleSharedRoutingModule
+  ]
+})
+export class ModuleSharedModule { }
